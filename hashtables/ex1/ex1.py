11823455e73de334de073452f6f650cb6a276b65
Billy Gingrich <whgingrich@gmail.com>
2020-05-08 12:20:55 -0400

    3 examples MVP

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,23 @@
+
+
+
+
 def get_indices_of_item_weights(weights, length, limit):
 
     """
     YOUR CODE HERE
     """
+    weight_dict = dict()
+
+    for i in range(0, length):
+        weight_dict[weights[i]] = i
+        
+    for i in range(0, length):
+        if limit - weights[i] in weight_dict:
+            weight_index1 = i
+            weight_index2 = weight_dict[limit - weights[i]]
+            if weight_index1 < weight_index2:
+                weight_index1, weight_index2 = weight_index2, weight_index1
+            return (weight_index1, weight_index2)
 
     return None
